c8c80a255578cdfa69312066771450199d0e8a2b
ANTiDOTE-21 <dtan7448@uni.sydney.edu.au>
2026-06-12 14:40:27 +1000

    a2_codebase

---
+++ A2code/aws/env.sh
@@ -0,0 +1,6 @@
+export AWS_REGION=us-east-1
+export S3_BUCKET_NAME=pdf-uploads-comp4349-dtan7448
+export FIXED_SIZE_QUEUE_URL=https://sqs.us-east-1.amazonaws.com/652291809448/fixed-size-chunking-queue
+export PARAGRAPH_AWARE_QUEUE_URL=https://sqs.us-east-1.amazonaws.com/652291809448/paragraph-aware-chunking-queue
+export DATABASE_URL='postgresql://postgres:password@pdf-db.chppvhvm8h7e.us-east-1.rds.amazonaws.com:5432/pdf-db'
+
